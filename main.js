5d0f8f1d7c1722a60bfaff2e62caa927ca3f44eb
Marta SIWINSKA <msiwinska@splio.com>
2020-05-26 01:09:14 +0200

    styling

--- main.js
+++ main.js
@@ -32,8 +32,17 @@ for (var i = 0; i < gameGrid.length; i++) {
 	var card = document.createElement('div');
 	card.classList.add('card');
 	card.dataset.name = gameGrid[i].name;
-	card.style.backgroundImage = `url(${gameGrid[i].img})`;
+	
+	var front = document.createElement('div');
+	front.classList.add('front');
+	
+	var back = document.createElement('div');
+	back.classList.add('back');
+	back.style.backgroundImage = `url(${gameGrid[i].img})`;
+	
 	grid.appendChild(card);
+	card.appendChild(front);
+	card.appendChild(back);
 }
 
 
@@ -79,12 +88,12 @@ grid.addEventListener('click', function(event) {
 		count++;
 		
 		if (count === 1) {
-			firstGuess = clicked.dataset.name;
-			clicked.classList.add('selected');
+			firstGuess = clicked.parentNode.dataset.name;
+			clicked.parentNode.classList.add('selected');
 		}
 		else {
-			secondGuess = clicked.dataset.name;
-			clicked.classList.add('selected');
+			secondGuess = clicked.parentNode.dataset.name;
+			clicked.parentNode.classList.add('selected');
 		}
 		
 		if (firstGuess !== '' && secondGuess !== '') {
